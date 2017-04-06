af26a45fa00a7f8427f65c765d86cdb17a0fe4ca
Alexey Drozdov <alexeydrozdov95@gmail.com>
2017-04-07 01:26:09 +0300

    game/feature: add hand and getting coins

--- game/src/states/nim.js
+++ game/src/states/nim.js
@@ -2,13 +2,18 @@ export default class Nim extends Phaser.State {
     create() {
         this.ROW_COUNT = 3;
         this.MAX_COINS_IN_ROW_COUNT = 5;
+        this.HAND_X = 400;
+        this.HAND_Y = 250;
         this.COIN_X = 100;
         this.COIN_Y = 100;
         this.COIN_OFFSET = 50;
         this.coins = [];
         this.currentLine = 0;
         this.counter = 0;
-
+        this.hand = this.add.sprite(this.HAND_X, this.HAND_Y, 'hand');
+        this.hand.inputEnabled = true;
+        this.hand.events.onInputDown.add(this.handListener, this);
+        
         for (let i = this.ROW_COUNT - 1; i > -1; i--){
             this.coins[i] = [];
             for (let j = 0; j < this.MAX_COINS_IN_ROW_COUNT - i; j++) {
@@ -18,9 +23,11 @@ export default class Nim extends Phaser.State {
             }
         }
     }
+
     coinsListener(...args) {
         if (this.currentLine === 0 || this.currentLine === args[0].position.y) {
             this.currentLine = args[0].position.y;
+            this.hand.loadTexture('hand-active', 0);
             
             if (args[0].key === 'coin') {
                 args[0].loadTexture('coin-selected', 0);
@@ -32,7 +39,23 @@ export default class Nim extends Phaser.State {
             
             if (this.counter === 0) {
                 this.currentLine = 0;
+                this.hand.loadTexture('hand', 0);
             }     
         }
+    }
+
+    handListener() {
+        if (this.hand.key === 'hand-active') {
+            let line = this.ROW_COUNT - (this.currentLine - this.COIN_Y) / this.COIN_OFFSET;
+            
+            this.coins[line].map((x, index) => {
+                if (x.key === 'coin-selected'){
+                    x.destroy();
+                }
+            })
+
+            this.currentLine = 0;
+            this.hand.loadTexture('hand', 0);
+        }   
     }
 }
