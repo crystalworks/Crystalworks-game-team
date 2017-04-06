a283f23591f51154c6cec1e2accb3d21f342f63c
Alexey Drozdov <alexeydrozdov95@gmail.com>
2017-04-07 01:19:30 +0300

    game/feature: add onclick event to coins

--- game/src/states/nim.js
+++ game/src/states/nim.js
@@ -6,13 +6,33 @@ export default class Nim extends Phaser.State {
         this.COIN_Y = 100;
         this.COIN_OFFSET = 50;
         this.coins = [];
+        this.currentLine = 0;
+        this.counter = 0;
 
         for (let i = this.ROW_COUNT - 1; i > -1; i--){
             this.coins[i] = [];
             for (let j = 0; j < this.MAX_COINS_IN_ROW_COUNT - i; j++) {
                 this.coins[i][j] = game.add.sprite(this.COIN_X + this.COIN_OFFSET * j, this.COIN_Y + this.COIN_OFFSET * (this.ROW_COUNT - i), 'coin');
+                this.coins[i][j].inputEnabled = true;
+                this.coins[i][j].events.onInputDown.add(this.coinsListener, this);
             }
         }
     }
-
+    coinsListener(...args) {
+        if (this.currentLine === 0 || this.currentLine === args[0].position.y) {
+            this.currentLine = args[0].position.y;
+            
+            if (args[0].key === 'coin') {
+                args[0].loadTexture('coin-selected', 0);
+                this.counter++;
+            } else {
+                args[0].loadTexture('coin', 0);  
+                this.counter--;
+            }    
+            
+            if (this.counter === 0) {
+                this.currentLine = 0;
+            }     
+        }
+    }
 }
--- game/src/states/preload.js
+++ game/src/states/preload.js
@@ -1,6 +1,7 @@
 export default class Preload extends Phaser.State {
     preload() {
         this.load.image('coin', 'assets/images/coin.png');
+        this.load.image('coin-selected', 'assets/images/coin-selected.png');
     }
 
     create() {
