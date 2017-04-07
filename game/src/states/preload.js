10e8fa4e4e5032a57d0860108c799e25df13c181
Irina <irina.buber@gmail.com>
2017-04-08 01:07:54 +0300

    game/feature : add boot-loader

--- game/src/states/preload.js
+++ game/src/states/preload.js
@@ -1,5 +1,12 @@
 export default class Preload extends Phaser.State {
     preload() {
+        this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg');
+        this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar');
+        this.loaderBg.anchor.setTo(0.5);
+        this.loaderBar.anchor.setTo(0.5);
+
+        this.load.setPreloadSprite(this.loaderBar);
+
         this.load.image('coin', 'assets/images/coin.png');
         this.load.image('coin-selected', 'assets/images/coin-selected.png');
         this.load.image('hand', 'assets/images/hand-no-active.png');
