27dac8245ce736d7bf39239baf091de3f5b03ffb
Alexey Drozdov <alexeydrozdov95@gmail.com>
2017-04-08 16:58:34 +0300

    game/feature: add backgroun image for nim

--- game/src/states/preload.js
+++ game/src/states/preload.js
@@ -4,6 +4,7 @@ export default class Preload extends Phaser.State {
         this.load.image('coin-selected', 'assets/images/coin-selected.png');
         this.load.image('hand', 'assets/images/hand-no-active.png');
         this.load.image('hand-active', 'assets/images/hand-active.png');
+        this.load.image('nim-background', 'assets/images/nim-background.svg');
     }
 
     create() {
