5ed4f5fcec9961c88eda7743a6d055cb31ef00b5
Irina <irina.buber@gmail.com>
2017-04-09 00:04:59 +0300

    game/feature: add menu-state

--- game/src/app.js
+++ game/src/app.js
@@ -3,6 +3,7 @@ import 'p2';
 import Phaser from 'phaser';
 
 import Boot from './states/boot';
+import Menu from './states/menu';
 import Preload from './states/preload';
 import Platformer from './states/platformer';
 import Nim from './states/nim';
@@ -18,9 +19,11 @@ class Game extends Phaser.Game {
         super(width, height, Phaser.CANVAS, 'content', null);
 
         this.state.add('Boot', Boot, false)
+        this.state.add('Menu', Menu, false)
         this.state.add('Preload', Preload, false)
         this.state.add('Platformer', Platformer, false)
         this.state.add('Nim', Nim, false)
+      
 
         this.state.start('Boot')
     }
