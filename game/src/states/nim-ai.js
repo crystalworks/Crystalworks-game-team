04cbc5a277d147dcfad6f88ea8ac77b84a47d9ce
Alexey Drozdov <alexeydrozdov95@gmail.com>
2017-04-08 12:44:48 +0300

    game/feature: add prepare logic for AI

--- game/src/states/nim-ai.js
+++ game/src/states/nim-ai.js
@@ -4,4 +4,62 @@
  * @return {object} 
  */
 export default function chooseMove(coins) {
+    let countCoinsInRow = getCountCoinsInRow(coins);
+    let countCoinsBit = countCoinsInRow.map(x => toBin(x));
+    let sumBits = getSumBit(countCoinsBit);
+    let count = toDec(sumBits);
+    let max = countCoinsInRow.reduce((acc, item) => {
+        if (acc < item){
+            return item;
+        }
+        else {
+            return acc;
+        }
+    }, 0);
+    let row = 0;
+}
+
+/**
+ * getCountCoinsInRow() returns count of coins in each row
+ * @param {array} coins
+ * @return {array} 
+ */
+function getCountCoinsInRow(coins) {
+    let countCoinsInRow = [];
+    coins.forEach( (x, index) => {
+        let count = 0;
+        x.forEach( y => {
+            if (y === 1)
+                count += 1; 
+        } )
+        countCoinsInRow[index] = count;
+    } )
+    return countCoinsInRow;
+}
+
+/**
+ * getSumBit() execute xor of counts coins in each row
+ * @param {array} binaryCountsCoins
+ * @return {array} 
+ */
+function getSumBit(binaryCountsCoins) {
+    let sumBits = [];
+    for (let i = 0; i < MAX_COUNT_BIT; i += 1) {
+        sumBits[i] = 0;
+        for (let j = 0; j < binaryCountsCoins.length; j += 1) {
+            sumBits[i] = sumBits[i] ^ binaryCountsCoins[j][i];
+        }
+    }
+    return sumBits.reverse();
+}
+
+/**
+ * toDec() convert binary number to decimal
+ * @param {array} binary
+ * @return {number} 
+ */
+function toDec(binary) {
+    return binary.reduce((acc, x, index) => {
+        return acc + x * Math.pow(2, index);     
+    })
 }
