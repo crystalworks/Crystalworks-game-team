ab7898e9b5df6e771d5c18d81360e284356231cd
Yev Dyko <evgeny.dyko@gmail.com>
2017-04-09 02:16:50 +0300

    game/feature: update preload state
    Loads all the game assets and starts the Game state.

--- game/src/states/preload.js
+++ game/src/states/preload.js
@@ -1,4 +1,8 @@
 export default class Preload extends Phaser.State {
+    init(levelData) {
+        this.levelData = levelData;
+    }
+
     preload() {
         this.loaderBg = this.add.sprite(
             this.game.world.centerX, 
@@ -20,6 +24,30 @@ export default class Preload extends Phaser.State {
         this.load.image('hand', 'assets/images/hand-no-active.png');
         this.load.image('hand-active', 'assets/images/hand-active.png');
         this.load.image('nim-background', 'assets/images/nim-background.svg');
+
+        const assets = this.levelData.assets;
+
+        for (let assetKey in assets) {
+            if (assets.hasOwnProperty(assetKey)) {
+                let asset = assets[assetKey];
+
+                switch (asset.type) {
+                    case 'image': {
+                        this.load.image(assetKey, asset.source);
+                        break;
+                    }
+                    case 'tilemap': {
+                        this.load.tilemap(
+                            assetKey, 
+                            asset.source, 
+                            null, 
+                            Phaser.Tilemap.TILED_JSON
+                        );
+                        break;
+                    }
+                } 
+            }
+        }
     }
 
     create() {
