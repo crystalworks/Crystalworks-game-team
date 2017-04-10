e88ef457824fe9508fdbc1fb21851a86c757ab08
IrinaBuber <irina.buber@gmail.com>
2017-04-11 00:42:38 +0300

    game/feature : add platform player

--- game/src/states/platformer.js
+++ game/src/states/platformer.js
@@ -1,3 +1,17 @@
-export default class Platformer extends Phaser.State {
-    create() {}
+import Player from './player.js';
+
+export default class Platformer extends Phaser.State {    
+    init() {
+
+    }
+    preload() {
+        this.load.image('sky', 'assets/images/sky.png');
+        this.load.image('ground', 'assets/images/platform.png');
+    }
+    create() {
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.add.sprite(0, 0, 'sky');
+        this.player = new Player(this.game, 32, this.world.height - 150);
+        this.add.group().add(this.player);        
+    }
 }
---
+++ game/src/states/player.js
@@ -0,0 +1,44 @@
+export default class Player extends Phaser.Sprite {
+    constructor(game, x, y) {
+        super(game, x, y, 'player');
+
+        this.animations.add("walking", [0, 1, 2, 1], 6, true);
+
+        this.game.physics.arcade.enable(this);
+
+        this.walking_speed = 200;
+        this.jumping_speed = 300;
+
+        this.frame = 3;
+        
+        this.body.gravity.y = 300;
+        this.body.collideWorldBounds = true;
+
+        this.anchor.setTo(0.5);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if (this.cursors.right.isDown && this.body.velocity.x >= 0) {
+            // move right
+            this.body.velocity.x = this.walking_speed;
+            this.animations.play("walking");
+            this.scale.setTo(-1, 1);
+        } else if (this.cursors.left.isDown && this.body.velocity.x <= 0) {
+            // move left
+            this.body.velocity.x = -this.walking_speed;
+            this.animations.play("walking");
+            this.scale.setTo(1, 1);
+        } else {
+            // stop
+            this.body.velocity.x = 0;
+            this.animations.stop();
+            this.frame = 3;
+        }
+
+        // jump only if touching a tile
+        if (this.cursors.up.isDown && this.body.blocked.down) {
+            this.body.velocity.y = -this.jumping_speed;
+        }
+    }
+}
