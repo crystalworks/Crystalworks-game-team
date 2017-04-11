d11ec30beb056b3a79e175e4073a68721b1f3093
Yev Dyko <evgeny.dyko@gmail.com>
2017-04-11 17:56:57 +0300

    game/feature: add platformer state
    Create map and set the tileset.

--- game/src/states/platformer.js
+++ game/src/states/platformer.js
@@ -1,3 +1,19 @@
 export default class Platformer extends Phaser.State {
-    create() {}
+    init() {
+        // start physics system
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.physics.arcade.gravity.y = 1000;
+    }
+    
+    create() {
+        // create map and set tileset
+        this.map = this.add.tilemap('platformer-tilemap');
+        this.map.addTilesetImage(
+            'tiles-spritesheet',
+            'tiles-spritesheet'
+        );
+        
+        this.layer = this.map.createLayer('collision');
+        this.layer.resizeWorld();
+    }
 }
