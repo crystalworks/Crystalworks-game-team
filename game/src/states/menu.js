c07872dcc48242b0a7b28c3530622738b8b82547
Irina <irina.buber@gmail.com>
2017-04-11 22:49:32 +0300

    game/ fix : update menu

--- game/src/states/menu.js
+++ game/src/states/menu.js
@@ -2,33 +2,37 @@ export default class Menu extends Phaser.State {
     init() { }
 
     preload() {
-        this.load.image('start-btn', 'assets/images/start-btn.png');
-        this.load.image('bonus-btn', 'assets/images/bonus-btn.png');      
+        this.load.image('start-btn', 'assets/images/start-btn.png'); 
     }
 
     create() {
         this.startBtn = this.add.button(
-            this.world.centerX - 300,
-            this.world.centerY - 50,
+            this.world.centerX - 100,
+            this.world.centerY + 50,
             'start-btn',
             this.gameLoad,
             this
         );
 
-        this.bonusBtn = this.add.button(
-            this.world.centerX + 50,
-            this.world.centerY - 50,
-            'bonus-btn',
-            this.bonusLoad,
-            this
+        this.title = game.add.text(
+            this.world.centerX - 125,
+            125,
+            'Nimomania'
         );
-    }
+        this.title.fontSize = 60;       
+        this.title.fill = '#fff';
+
+        this.info = game.add.text(
+            this.world.centerX - 125,
+            210,
+            'collect coins to play with master'
+        );
+        this.info.fontSize = 20;       
+        this.info.fill = '#fff';
+ }
 
     gameLoad() {
         this.state.start('Preload');
     }
 
-    bonusLoad() {
-        this.state.start('Preload');
-    }
 }
