7efb7f51b5aa69140866f11d777dbcdf047a6309
Yev Dyko <evgeny.dyko@gmail.com>
2017-04-11 16:29:03 +0300

    game/fix: update boot state

--- game/src/states/boot.js
+++ game/src/states/boot.js
@@ -1,21 +1,16 @@
 import Phaser from 'phaser-ce';
 
 export default class Boot extends Phaser.State {
-    init(levelFile) {
+    init() {
         this.stage.backgroundColor = '#000';
-        this.levelFile = levelFile;
     }
 
     preload() {
         this.load.image('loaderBg', 'assets/images/loader-bg.png');
         this.load.image('loaderBar', 'assets/images/loader-bar.png');
-        this.load.text('platformer', this.levelFile);
     }
 
     create() {
-        const levelText = this.cache.getText('platformer');
-        const levelData = JSON.parse(levelText);
-
-        this.state.start('Preload', true, false, levelData);
+        this.state.start('Preload');
     }   
 }
