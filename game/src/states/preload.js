b979be525e926b2957f9e41f9bb4f38519915249
Alexey Drozdov <alexeydrozdov95@gmail.com>
2017-04-13 17:51:48 +0300

    game/fix: fix nim background

--- game/src/states/preload.js
+++ game/src/states/preload.js
@@ -41,7 +41,7 @@ export default class Preload extends Phaser.State {
         this.load.image('coin-selected', 'assets/images/coin-selected.png');
         this.load.image('hand', 'assets/images/hand-no-active.png');
         this.load.image('hand-active', 'assets/images/hand-active.png');
-        this.load.image('nim-background', 'assets/images/nim-background.svg');
+        this.load.image('nim-background', 'assets/images/nim-background.png');
 
         this.load.audio('nim-get-coins', 'assets/audio/sounds/nim-get-coins.wav');
     }
