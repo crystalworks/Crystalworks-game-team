303b161c98f664a71a2040df01c3a8307a8e532e
Yev Dyko <evgeny.dyko@gmail.com>
2017-04-13 13:30:06 +0300

    game/fix: update nim graphics

--- game/src/states/preload.js
+++ game/src/states/preload.js
@@ -28,7 +28,7 @@ export default class Preload extends Phaser.State {
         this.load.image('coin-selected', 'assets/images/coin-selected.png');
         this.load.image('hand', 'assets/images/hand-no-active.png');
         this.load.image('hand-active', 'assets/images/hand-active.png');
-        this.load.image('nim-background', 'assets/images/nim-background.svg');
+        this.load.image('nim-background', 'assets/images/background-wood.png');
         this.load.image('sky', 'assets/images/sky.png');
 
         this.load.tilemap(
